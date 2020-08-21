011f61c1d10f7afe700510ffb2497b7221a03c51
maneshmohanan1 <68895389+maneshmohanan1@users.noreply.github.com>
2020-08-21 11:28:21 +0400

    azure
    azure

---
+++ azure/storageAccount/outputs.tf
@@ -0,0 +1,13 @@
+#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
+#*   Storage accounts -  Outputs                       *#
+#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
+
+output "storage_account_name" {
+    description    =    "Name of the storage accounts"
+    value          =    azurerm_storage_account.sa.*.name
+}
+
+output "storage_container_name" {
+    description    =    "Name of the storage containers"
+    value          =    azurerm_storage_container.sc.*.name
+}
